1e35f858c513ce188c41f8d44e5fa6ee3c1dd331
Avishek Verma <avishekverma298@gmail.com>
2023-06-29 23:11:12 +0530

    JDBC Adv-1 code

---
+++ JDBC_Adv_1/JDBCBlobOperationApp/src/com/avishek/main/BlobInsertionApp.java
@@ -0,0 +1,73 @@
+package com.avishek.main;
+
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import com.avishek.util.JdbcUtil;
+
+public class BlobInsertionApp {
+
+	public static void main(String[] args) {
+		
+		// Resources used 
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		Scanner scanner = null;
+		
+		// Variables used
+		String name = null;
+		String imageLoc = null;
+				
+		
+		try {
+			connection = JdbcUtil.getJdbcConnection();
+			
+			String sqlInsetQuery = "insert into persons(`name`,`image`)values(?,?)";
+			if(connection != null) {
+				pstmt = connection.prepareStatement(sqlInsetQuery);
+			}
+			
+			if(pstmt != null) {
+				scanner = new Scanner(System.in);
+				
+				// Collecting the inputs
+				if(scanner != null) {
+					System.out.println("Enter the username ::");
+					name = scanner.next();
+					
+					System.out.println("Enter the image location :: ");
+					imageLoc = scanner.next();
+				}
+				
+				// Set the input values to Query
+				pstmt.setString(1, name);
+				pstmt.setBinaryStream(2, new FileInputStream(new File(imageLoc)));
+				
+				// Execute the query
+				int rowAffected = pstmt.executeUpdate();
+				System.out.println("No of row inserted is ::"+rowAffected);
+			}
+		}catch(SQLException | IOException e) {
+			e.printStackTrace();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			
+			try {
+				JdbcUtil.cleanUp(connection, pstmt, null);
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+			scanner.close();
+		}
+				
+
+	}
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Advance_Java_JDBC_Notes
 
+JDBC Topics Covered
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+7. 
+8.
+9.
+10.
+
+
